d674f3a468f57114ad12605aeb428ec01ce235b9
TheWhiteDevil0831 <62797037+TheWhiteDevil0831@users.noreply.github.com>
2020-04-10 02:20:37 +0530

    Update Python Code.py

--- Code Files/Python Code.py
+++ Code Files/Python Code.py
@@ -6,8 +6,8 @@ ser = serial.Serial('COM4', 9600)
 
 
 # Your Account Sid and Auth Token from twilio.com/console
-account_sid = 'ACd20a6f020efba34b6e87e57cc629fb6a'
-auth_token = '148c2a129569b28cc2fdd76b1f1291fa'
+account_sid = 'ACd20a6f020efba34b6e87e57cc629fb6a'         # here put your ACCOUNT SID  
+auth_token = '148c2a129569b28cc2fdd76b1f1291fa'            # here put your AUTH TOKEN
 client = Client(account_sid, auth_token)
 
 
@@ -18,7 +18,7 @@ while True:
         message = client.messages.create(
                               body=messageTosend,
                               from_='whatsapp:+14155238886',
-                              to='whatsapp:+919538515360'
+                              to='whatsapp:+919538515360'        # here put your Whatsapp number added in Twilio Account
                           )
         time.sleep(10)
 
